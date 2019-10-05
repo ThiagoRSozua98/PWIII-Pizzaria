3edc8bd124f3b0d2b87f3889f9f9a10700bc8011
Thiago/Juan <thiago.reiissouza@gmail.com>
2019-10-04 21:34:41 -0300

    Exercicio 7

--- src/app/comanda/comanda.component.ts
+++ src/app/comanda/comanda.component.ts
@@ -8,9 +8,17 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class ComandaComponent implements OnInit {
 
+  FormComanda: FormGroup;
+
   constructor(public formBuilder: FormBuilder) {  }
 
   ngOnInit() {
+    this.FormComanda= this.formBuilder.group({
+      chopp: this.formBuilder.control(''),
+      pizza: this.formBuilder.control(''),
+      recheio: this.formBuilder.control(''),
+      pessoa: this.formBuilder.control(''),
+      taxa: this.formBuilder.control( true)
+    })
   }
-
 }
